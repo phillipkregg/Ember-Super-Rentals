0eb7cc4f9122eb711d9bc2774e5a8771be8cd4cb
Phillip Lackey <dp078971@MBP-C02NJ4G4G3QN.local>
2016-05-30 22:49:58 -0500

    created custom handlebars helper

---
+++ app/helpers/rental-property-type.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+const communityPropertyTypes = [
+  'Condo',
+  'Townhouse',
+  'Apartment'
+];
+
+export function rentalPropertyType([type]/*, hash*/) {
+  if (communityPropertyTypes.contains(type)) {
+    return 'Community';
+  }
+}
+
+export default Ember.Helper.helper(rentalPropertyType);
